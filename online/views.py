01869891a98107bf9da59cf1a195edcec554e0d3
ekkazit <ekkazit@gmail.com>
2018-01-10 09:02:09 +0700

    Add online course

--- online/views.py
+++ online/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Video
 
@@ -13,6 +13,27 @@ def index(request):
 
 
 def detail(request, slug):
+    video = get_object_or_404(Video, slug=slug)
+    ids = video.tags.values_list('id', flat=True)
+    related_videos = Video.objects.filter(tags__in=ids).distinct().exclude(id=video.id)[:5]
+
+    autoplay = False
+    player= None
+    pl = request.GET.get('pl')
+    active_pl = -1
+    if pl:
+        autoplay = True
+        player= video.videoplaylist_set.filter(id=int(pl)).first()
+    else:
+        player = video.videoplaylist_set.first()
+        if player:
+            active_pl = player.id
+
     return render(request, 'online/detail.html', {
         'menu': 'online',
+        'video': video,
+        'related_videos': related_videos,
+        'player': player,
+        'autoplay': autoplay,
+        'active_pl': active_pl,
     })
