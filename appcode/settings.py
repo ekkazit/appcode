4e847256b0bc5facc7aeffe7cccecd6e30d17d28
ekkazit <ekkazit@gmail.com>
2018-01-16 20:54:19 +0700

    Add fixed layout

--- appcode/settings.py
+++ appcode/settings.py
@@ -131,5 +131,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'appcodetraining@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'p@$$word'
 DEFAULT_FROM_EMAIL = 'appcodetraining@gmail.com'
