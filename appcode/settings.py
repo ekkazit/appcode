04043a1c67d8e7b2e44a35928f8aca4bd1f97a4d
ekkazit <ekkazit@gmail.com>
2018-01-16 20:57:03 +0700

    Fixed secure

--- appcode/settings.py
+++ appcode/settings.py
@@ -131,5 +131,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'appcodetraining@gmail.com'
-EMAIL_HOST_PASSWORD = 'p@$$word'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'appcodetraining@gmail.com'
